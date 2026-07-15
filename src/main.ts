b215e01293eeb0f733ae60b7234aecace5316bb4
rdjanuar <rdjanuar008@gmail.com>
2026-07-15 10:55:17 +0700

    feat: append transactionId to binding redirect URL and fix whitespace formatting

--- src/main.ts
+++ src/main.ts
@@ -57,11 +57,15 @@ function buildExistingMyTelkomselUrl({
 
     if (urlObj.searchParams.has('type')) {
       const type = urlObj.searchParams.get('type')
-      if  (type === 'binding') {
+      if (type === 'binding') {
         const bindingPage = urlObj.searchParams.get('redirectPage') || ''
         const decodedPathBindingPage = decodeURIComponent(bindingPage || "");
         const hasProtocol = bindingPage.startsWith("http://") || bindingPage.startsWith("https://");
         const redirectBindingPage = new URL(hasProtocol ? decodedPathBindingPage : `https://${decodedPathBindingPage}`)
+
+        if (transactionId && !urlObj.searchParams.has('transactionId')) {
+          redirectBindingPage.searchParams.set('transactionId', transactionId)
+        }
 
         return redirectBindingPage.toString()
       }
@@ -75,7 +79,7 @@ function buildExistingMyTelkomselUrl({
       urlObj.searchParams.set("refreshBalance", refreshBalance);
     }
 
-   
+
     return urlObj.toString();
   } catch (e) {
     return hasProtocol ? decodedPath : `https://${decodedPath}`;
